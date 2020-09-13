489fdab17e9d6a1b06d53aae8440829fdd90da2e
vladkotulla <71208449+vladkotulla@users.noreply.github.com>
2020-09-13 12:39:08 +0300

    Project commit

---
+++ My first platform game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+    }
+}
---
+++ My first platform game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Animator animator;
+    private Rigidbody2D rigidbody;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator.Play("Idle", 0, 0.25f);
+    }
+
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+
+        rigidbody.MovePosition(rigidbody.position + Vector2.right * moveX * speed * Time.deltaTime);
+    }
+}
